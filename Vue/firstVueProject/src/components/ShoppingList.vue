0ffe5d2b483e56f0eceab7191405a8df7ea4fc4c
Evin <evinpbg@gmail.com>
2023-11-02 14:37:40 -0400

    project practice

---
+++ Vue/firstVueProject/src/components/ShoppingList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <header>
+        <h2>Shopping List</h2>
+      </header>
+      <input type="text" 
+      ref="inputValue"
+      v-model="inputValue"
+      @keyup.enter="addItem"/>
+      <button @click="addItem">Add Item</button>
+      <ul>
+        <li v-for="(item, i) in shoppingList"
+        :key="i">
+        {{item}}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputValue: '',
+        shoppingList: []
+      }
+    },
+    methods: {
+        addItem() {
+            if(this.inputValue != '') {
+                this.shoppingList.push(this.inputValue);
+                this.inputValue = '';
+            }
+            this.$refs.inputValue.focus();
+            console.log(this.shoppingList);
+        }
+    }
+  }
+  </script>
+  
+  <style></style>
