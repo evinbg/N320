2c02d54cea4c79df336ec3b29e5b4563108fa45e
Evin <evinpbg@gmail.com>
2023-11-07 14:41:49 -0500

    created props project

---
+++ Vue/propsSlots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>Props and Slots</h1>
+<div>
+  <props-one 
+  v-bind:greeting="greeting"
+  :who="who"
+  :myVue="myVue"
+  msg="Props are awesome!"></props-one>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'hello',
+      who: 'Vue',
+      count: 1,
+      myVue: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
