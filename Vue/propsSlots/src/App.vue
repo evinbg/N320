3a5238aed681411648314d299e5463f65af32e29
Evin Bour-Gilson <evinpbg@gmail.com>
2023-11-14 01:44:26 -0500

    more details about props

--- Vue/propsSlots/src/App.vue
+++ Vue/propsSlots/src/App.vue
@@ -6,6 +6,18 @@
   :who="who"
   :myVue="myVue"
   msg="Props are awesome!"></props-one>
+  <props-two v-bind="book"></props-two>
+</div>
+<div>
+  <h1>Student Data</h1>
+  <student-data 
+  v-for="student in students" :key="student.sName"
+  :sName="student.sName" 
+  :sAge="student.sAge" 
+  :major="student.major"
+  :cLevel="student.cLevel"
+  :graduate="student.graduate"
+  ></student-data>
 </div>
 </template>
 
@@ -16,7 +28,25 @@ export default {
       greeting: 'hello',
       who: 'Vue',
       count: 1,
-      myVue: 'Vue.js'
+      myVue: 'Vue.js',
+      book: {
+        id: 1,
+        title: "Vue 3 Development"
+      },
+      students: [
+        {
+          sName: 'Robert Smith',
+          sAge: 22,
+          major: "Web Development",
+          cLevel: 3,
+        },
+        {
+          sName: 'Susan Rutherford',
+          sAge: 25,
+          major: "Informatics",
+          graduate: true
+        }
+      ]
     }
   }
 }
--- Vue/propsSlots/src/main.js
+++ Vue/propsSlots/src/main.js
@@ -1,8 +1,12 @@
 import PropsOne from './components/propsOne.vue';
+import PropsTwo from './components/propsTwo.vue';
+import StudentData from './components/studentData.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 const app = createApp(App);
 app.component('props-one', PropsOne);
+app.component('props-two', PropsTwo);
+app.component('student-data', StudentData);
 app.mount('#app');
